d67fad62afa49ef0e0a62705423463407a007864
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2025-06-09 03:41:08 +0330

    __dirname

--- compiler/flux.js
+++ compiler/flux.js
@@ -1,11 +1,16 @@
 // flux.js
 
 import { readFileSync } from 'node:fs';
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { tokenize } from './tokenizer.js';
 import { Parser } from './parser.js';
 import { Interpreter } from './interpreter.js';
 
-const code = readFileSync('index.flux', 'utf-8');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const code = readFileSync(resolve(__dirname, '../index.flux'), 'utf-8');
 
 const tokens = tokenize(code);
 const parser = new Parser(tokens);
@@ -13,6 +18,3 @@ const ast = parser.parse();
 
 const interpreter = new Interpreter(ast);
 interpreter.run();
-
-console.log('✅  Execution finished.');
-// console.log(JSON.stringify(interpreter.environment, null, 2));
