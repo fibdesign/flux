c764b0cbc0f3a467aae4180f02cf89fe5a919f44
Sadegh <74778568+msghaderi77@users.noreply.github.com>
2025-06-18 00:38:55 +0330

    Update logger.js

--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,6 @@
 // utils/logger.js
 
+const FluxErrors = require("./FluxErros");
 const Reset = '\x1b[0m';
 const FgGreen = '\x1b[32m';
 const FgYellow = '\x1b[33m';
@@ -26,4 +27,38 @@ function logSpinner(msg) {
     console.error(msg);
 }
 
-module.exports = { logSpinner,logSuccess,logInfo,logWarning,logError };
+function throwTypeError(err, meta = undefined) {
+    if (meta){
+        console.log('this is meta:',meta)
+    }
+    FluxErrors.type({
+        message: err,
+        file: meta?.file ?? 'unknown file',
+        line: meta?.line ?? 'unknown',
+        column: meta?.column ?? 'unknown',
+        code: `    ${meta?.code}`
+    });
+}
+function throwError(err, meta = undefined) {
+    if (meta){
+        console.log('this is meta:',meta)
+    }
+    FluxErrors.runtime({
+        message: err,
+        file: meta?.file ?? 'unknown file',
+        line: meta?.line ?? 'unknown',
+        column: meta?.column ?? 'unknown',
+        code: `    ${meta?.code}`
+    });
+}
+function throwSyntaxError(err, meta = undefined) {
+    FluxErrors.syntax({
+        message: err,
+        file: meta?.file ?? 'unknown file',
+        line: meta?.line ?? 'unknown',
+        column: meta?.column ?? 'unknown',
+        code: `    ${meta?.code}`
+    });
+
+}
+module.exports = { logSpinner,logSuccess,logInfo,logWarning,logError,throwTypeError,throwError,throwSyntaxError };
